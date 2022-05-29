c18e9efe7e62c872be61d809d13ff679c547fe97
Jesus Esquivel Roman <jesquivelr@outlook.com>
2022-05-29 18:59:34 +0100

    First commit from AWS Amplify tutorial
    Signed-off-by: Jesus Esquivel Roman <jesquivelr@outlook.com>

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "RestaurantAPI": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "amplifyapp93516781": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
